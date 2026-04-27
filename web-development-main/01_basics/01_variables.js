348736240456955ee988da656b9653b55d2afbbc
Aman <amankumar21752@gmail.com>
2026-04-27 21:19:30 +0530

    Add files via upload

---
+++ web-development-main/01_basics/01_variables.js
@@ -0,0 +1,26 @@
+const accountId = 14453 // constant
+let accountEmail = "Ak@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi" // not good method to declare variable.
+let accountState // this is our choice to put semicolon(;) or not.
+
+
+// accountId = 2 // const variable not allow to change 
+
+accountEmail = "hc@hc.com"
+accountPassword = "423431134"
+accountCity = "Gujrat"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+// let and var used to declare variable , but nowdays only let keyword is used for declaring variables.
+//  {} = > scope
+
+// used only const and let.
+
+/* 
+NOT USED var keyword. 
+var have issued in block scope and functional scope.
+*/
